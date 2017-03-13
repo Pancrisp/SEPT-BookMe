3a3f2690a92dfdd68e82e5aef14e351099a0f988
Pancrisp <ervinchua@gmail.com>
2017-03-13 21:13:43 +1100

    Added views for login/registration pages

---
+++ laravel/resources/views/signup.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Restaurant Booking App</title>
+
+        <!-- Font Awesome -->
+        <link href="vendor/font-awesome-4.6.3/css/font-awesome.min.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="css/main.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <div class="login">
+                <p>Already have an account? <a href="/">Sign in here</a></p>
+            </div>
+        </div>
+    </body>
+
+</html>
