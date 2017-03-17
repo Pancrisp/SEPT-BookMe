cc5eac5e2e11297095c445d1d0701a692d54586a
Grace Zheng <s3558319@student.rmit.edu.au>
2017-03-18 03:22:02 +1100

    added modals and created database tables 	new file:   app/Booking.php 	new file:   app/Business.php 	new file:   app/Customer.php 	new file:   database/migrations/2017_03_17_150600_create_customers_table.php 	new file:   database/migrations/2017_03_17_150848_create_businesses_table.php 	new file:   database/migrations/2017_03_17_150942_create_bookings_table.php

---
+++ laravel/database/migrations/2017_03_17_150600_create_customers_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * **CUSTOMERS**
+     * -id (primary key)
+     * -customer_name
+     * -username
+     * -password
+     * -mobile_phone
+     * -email_address
+     * -address
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('customer_name');
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->string('mobile_phone');
+            $table->string('email_address');
+            $table->text('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
