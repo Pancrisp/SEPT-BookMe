53ddc4432e1141ad3af4950a58d50dc84271e61b
Grace Zheng <s3558319@student.rmit.edu.au>
2017-04-05 01:13:50 +1000

    made booking seeder more effective 	modified:   database/seeds/BookingsTableSeeder.php

--- laravel/database/seeds/BookingsTableSeeder.php
+++ laravel/database/seeds/BookingsTableSeeder.php
@@ -13,24 +13,41 @@ class BookingsTableSeeder extends Seeder
      */
     public function run()
     {
-        $today = Carbon::today();
-        $hour = rand(9, 13);
+        $date = Carbon::today();
 
         $noOfCustomers = \App\Customer::count();
 
-        for($i=0; $i<10; $i++){
-            $customer = rand(1, $noOfCustomers);
-            $business = rand(1,3);
+        for($i=0; $i<5; $i++){
+            $hour = rand(9, 13);
+            $date = $date->addDay();
 
             Booking::create([
-                'date' => $today->toDateString(),
+                'date' => $date->toDateString(),
                 'start_time' => Carbon::createFromTime($hour, 0)->toTimeString(),
                 'end_time' => Carbon::createFromTime($hour, 30)->toTimeString(),
-                'customer_id' => $customer,
-                'business_id' => $business,
+                'customer_id' => rand(1, $noOfCustomers),
+                'business_id' => rand(1,3),
             ])->save();
 
             $hour += rand(1, 2);
+
+            Booking::create([
+                'date' => $date->toDateString(),
+                'start_time' => Carbon::createFromTime($hour, 0)->toTimeString(),
+                'end_time' => Carbon::createFromTime($hour, 30)->toTimeString(),
+                'customer_id' => rand(1, $noOfCustomers),
+                'business_id' => rand(1,3),
+            ])->save();
+
+            $hour += rand(1, 2);
+
+            Booking::create([
+                'date' => $date->toDateString(),
+                'start_time' => Carbon::createFromTime($hour, 0)->toTimeString(),
+                'end_time' => Carbon::createFromTime($hour, 30)->toTimeString(),
+                'customer_id' => rand(1, $noOfCustomers),
+                'business_id' => rand(1,3),
+            ])->save();
         }
 
     }
