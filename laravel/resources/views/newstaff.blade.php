168e6ac4a37b47bac9a7af4ba00f764de42ec9f9
Ervin <ervinchua@gmail.com>
2017-04-04 19:31:04 +1000

    Use associative arrays to store available days in form

--- laravel/resources/views/newstaff.blade.php
+++ laravel/resources/views/newstaff.blade.php
@@ -12,28 +12,27 @@
         <form action="/addstaff" method="post">
             {{ csrf_field() }}
             <input type="text" name="business_id" value="{{ $data['id'] }}" hidden>
-            <input type="text" name="fullname" placeholder="Full Name" value="{!! old('fullname') !!}">
-            <input type="text" name="taxfileno" placeholder="TFN" value="{!! old('TFN') !!}">
-            <input type="text" name="phone" placeholder="Contact No" value="{!! old('phone') !!}">
-            <select name="role" placeholder="Role">
+            <input type="text" name="fullname" placeholder="Full Name" value="{!! old('fullname') !!}" required>
+            <input type="text" name="taxfileno" placeholder="TFN" value="{!! old('TFN') !!}" required>
+            <input type="text" name="phone" placeholder="Contact No" value="{!! old('phone') !!}" required>
+            <select name="role" placeholder="Role" required>
                 <option value="" selected disabled>Select role</option>
                 <option value="{!! old('role') !!}">Waiter</option>
             </select>
             <h4>Available working days</h4>
             <div class="days-selection">
                 <div class="days">
-                    <label><input type="checkbox" name="Monday" value="Mon">Monday</label>
-                    <label><input type="checkbox" name="Tuesday" value="Tue">Tuesday</label>
-                    <label><input type="checkbox" name="Wednesday" value="wed">Wednesday</label>
-                    <label><input type="checkbox" name="Thursday" value="Thu">Thursday</label>
-                    <label><input type="checkbox" name="Friday" value="Fri">Friday</label>
+                    <label><input type="checkbox" name="availability[]" value="Mon">Monday</label>
+                    <label><input type="checkbox" name="availability[]" value="Tue">Tuesday</label>
+                    <label><input type="checkbox" name="availability[]" value="Wed">Wednesday</label>
+                    <label><input type="checkbox" name="availability[]" value="Thu">Thursday</label>
+                    <label><input type="checkbox" name="availability[]" value="Fri">Friday</label>
                 </div>
                 <div class="days">
-                    <label><input type="checkbox" name="Saturday" value="Sat">Saturday</label>
-                    <label><input type="checkbox" name="Sunday" value="Sun">Sunday</label>
+                    <label><input type="checkbox" name="availability[]" value="Sat">Saturday</label>
+                    <label><input type="checkbox" name="availability[]" value="Sun">Sunday</label>
                 </div>
             </div>
-
 
             <button type="submit" name="submit">Submit</button>
         </form>
