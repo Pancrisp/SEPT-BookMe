f67c4e51887077a7f71ac0b5bf92a89312d887e8
Grace Zheng <s3558319@student.rmit.edu.au>
2017-05-09 11:05:26 +1000

    added new model and table 	new file:   app/BusinessHour.php 	new file:   database/migrations/2017_05_09_005735_create_business_hours_table.php

---
+++ laravel/database/migrations/2017_05_09_005735_create_business_hours_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBusinessHoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * -business_hour_id (pk)
+     * -opening_hour
+     * -closing_hour
+     * -business_id (fk)
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('business_hours', function (Blueprint $table) {
+            $table->increments('business_hour_id');
+            $table->string('day');
+            $table->string('opening_hour')->default('00:00:00');
+            $table->string('closing_hour')->default('00:00:00');
+            $table->integer('business_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('business_hours');
+    }
+}
