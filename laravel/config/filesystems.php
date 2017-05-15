3f11a069ddda344125c9810d7ba65a4755172ab7
Grace Zheng <s3558319@student.rmit.edu.au>
2017-05-15 22:31:47 +1000

    changed accidental refactors back 	modified:   config/database.php 	modified:   config/filesystems.php 	modified:   config/mail.php 	modified:   public/js/app.js

--- laravel/config/filesystems.php
+++ laravel/config/filesystems.php
@@ -35,7 +35,7 @@ return [
     |
     | Here you may configure as many filesystem "disks" as you wish, and you
     | may even configure multiple disks of the same driver. Defaults have
-    | been register for each driver as an example of the required options.
+    | been setup for each driver as an example of the required options.
     |
     | Supported Drivers: "local", "ftp", "s3", "rackspace"
     |
